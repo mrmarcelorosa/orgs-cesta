2293b0cc109c1081d9b4e8bbedea0b6256a3cee5
Marcelo Rosa <vyctor.rosa14@gmail.com>
2023-02-13 21:41:33 -0300

    create component Cesta

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar, SafeAreaView } from 'react-native';
+import React from 'react';
+import Cesta from './src/telas/Cesta';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView >
+      <StatusBar/>
+      <Cesta></Cesta>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
